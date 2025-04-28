253017db1c72f4d31bf74fa0e866141f7066e3e3
215569StephanieJaime <al215569@alumnos.uacj.mx>
2025-04-27 22:30:31 -0600

    Cambios en el build.sh y render.yml

--- app/build.sh
+++ app/build.sh
@@ -10,7 +10,7 @@ python manage.py collectstatic --no-input --clear
 # Aplicar migraciones
 python manage.py migrate
 
-# Crear superusuario con username
+# Crear superusuario con username (versión confirmada como funcional)
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -20,6 +20,6 @@ if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
         email='$DJANGO_SUPERUSER_EMAIL',
         password='$DJANGO_SUPERUSER_PASSWORD'
     )
-    print(f'Superusuario {DJANGO_SUPERUSER_USERNAME} creado')
+    print(f'Superusuario {$DJANGO_SUPERUSER_USERNAME} creado')
 else:
-    print(f'El usuario {DJANGO_SUPERUSER_USERNAME} ya existe')"
+    print(f'El usuario {$DJANGO_SUPERUSER_USERNAME} ya existe')"
